6fc01bae99e8efb4e327c323cfc2f62fcdef0923
Sandor Teleki <sandorteleki1@gmail.com>
2026-01-05 15:05:17 +0100

    fix: refactor done

--- dist/command_exit.js
+++
@@ -1,5 +0,0 @@
-export function commandExit() {
-    console.log("Closing the Pokedex... Goodbye!");
-    process.exit(0);
-}
-;
--- dist/command_help.js
+++
@@ -1,7 +0,0 @@
-export function commandHelp(commands) {
-    console.log("Welcome to the Pokedex!");
-    console.log("Usage:\n");
-    for (const command of Object.values(commands)) {
-        console.log(`${command.name}: ${command.description}`);
-    }
-}
